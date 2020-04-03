01ff4123fce6bb61d633120b3c872629dcd241fe
Mohamed Ali Dell Xps <mohamedalibouzrati@gmail.com>
2020-04-03 14:34:05 +0200

    Correct last bugs

--- src/main/webapp/app/layouts/navbar/navbar-admin/navbar-admin.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar-admin/navbar-admin.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'app/layouts/profiles/profile.service';
 
 @Component({
   selector: 'jhi-navbar-admin',
   templateUrl: './navbar-admin.component.html'
 })
-export class NavbarAdminComponent {
+export class NavbarAdminComponent implements OnInit {
   isNavbarCollapsed = true;
+  swaggerEnabled?: boolean;
+  inProduction?: boolean;
+
+  constructor(private profileService: ProfileService) {}
 
   collapseNavbar(): void {
     this.isNavbarCollapsed = true;
+  }
+
+  ngOnInit(): void {
+    this.profileService.getProfileInfo().subscribe(profileInfo => {
+      this.inProduction = profileInfo.inProduction;
+      this.swaggerEnabled = profileInfo.swaggerEnabled;
+    });
   }
 }
